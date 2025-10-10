0a29dbf14899cbd5c3b80dad7feef0ddf82f33f7
Al zaman <alzamanpoothakkal@gmail.com>
2025-10-10 20:55:09 +0530

    improving

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss(),],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // prevents excessive code splitting
+      },
+    },
+  },
+});
